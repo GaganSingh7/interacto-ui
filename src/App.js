d59bbded327a3f8bf79d1701f997ad180003f916
gaga8820 <gagandeep.singh1@rackspace.com>
2023-01-20 15:46:03 +0530

    update home page text

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ function App() {
     <div className={styles.App}>
       <header className={styles.App_header}>
       <div className={styles.wrapper}>
-        <h1 className={styles.heading}>Coming soon.</h1>
-        <h2>We are building our website.</h2>
+        <h1 className={styles.heading}>Interacto! Coming soon.</h1>
+        <h2>We are building a platform.</h2>
         <div className={styles.loading}><div></div><div></div><div></div></div>
       </div>
       </header>
